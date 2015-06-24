2eddcea89c46b74b20e89bdc718733ce2c5ed1b1
Murray Smith <murray@businessinstincts.com>
2015-06-24 12:33:04 -0600

    improve email templating + reminders

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 #!/bin/env node
 
-var express  = require('express'),
+var FB_BASE_URL = 'https://roster-io.firebaseio.com';
+
+var fs       = require('fs'),
+	express  = require('express'),
 	server   = express(),
 	Postmark = require('postmark'),
 	postmark = new Postmark.Client('75cdd97a-2c40-4319-a6a9-4576d0948d57'),
-	fs       = require('fs');
+	ejs      = require('ejs'),
+	juice    = require('juice'),
+	moment   = require('moment'),
+	Firebase = require('firebase');
 
 // web app
 server.use(express.static('html'));
@@ -26,14 +32,67 @@ server.post('/api/v1/email', function (req, res) {
 		res.json({success: true});
 	});
 });
-server.get('/api/v1/email/reminder', function (req, res) {
-	fs.readFile('emails/reminder.html', function (err, data) {
+server.get('/api/v1/email/:template', function (req, res) {
+	// read template
+	fs.readFile('html/emails/' + req.params.template + '.html', function (err, file) {
 		if (err) {
 			console.error(err);
 			return;
 		}
-		res.type('text/html');
-		res.send(data);
+		
+		// fetch data
+		new Firebase(FB_BASE_URL + '/rosters/' + req.query.roster).on('value', function (rosterSnap) {
+			var roster  = rosterSnap.val(),
+				event   = roster.events[req.query.event],
+				subject = 'Reminder: RSVP Required - ' + roster.name + ' - ' + event.name;
+			
+			// render template
+			var template = ejs.compile(file.toString());
+			var html = template({
+				subject: subject,
+				roster:  roster,
+				event:   event,
+				
+				url:     'http://rstr.io/#/roster/' + req.query.roster + '/' + req.query.event,
+				query:   req.query,
+				moment:  moment,
+			});
+			
+			// juice email
+			juice.juiceResources(html, {}, function (err, html2) {
+				if (err) {
+					console.error(err);
+					return;
+				}
+				
+				if (1) {
+					// gather recipients
+					var to = 'Murray Smith <murray@mismith.info>';
+					
+					// send email
+					postmark.sendEmail({
+						From: 'rsvp@rstr.io',
+						To: to,
+						Subject: subject, 
+						HtmlBody: html,
+						TrackOpens: true,
+					}, function (err, data) {
+		/*
+						if (err) {
+							res.json({error: err});
+							return;
+						}
+						res.json({success: true});
+		*/
+					});
+				}
+				
+				// return response
+				res.type('text/html');
+				res.send(html);
+			});
+			
+		});
 	});
 });
 
