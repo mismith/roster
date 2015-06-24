60f3b86c81422dc7f4b591b33bbd75c8708a6c72
Murray Smith <murray@businessinstincts.com>
2015-06-23 23:45:19 -0600

    basic email template started

--- server.js
+++ server.js
@@ -3,14 +3,15 @@
 var express  = require('express'),
 	server   = express(),
 	Postmark = require('postmark'),
-	postmark = new Postmark.Client('75cdd97a-2c40-4319-a6a9-4576d0948d57');
+	postmark = new Postmark.Client('75cdd97a-2c40-4319-a6a9-4576d0948d57'),
+	fs       = require('fs');
 
 // web app
 server.use(express.static('html'));
 server.get('/', express.static('html/index.html'));
 
 // api
-server.post('/api/v1/notify/email', function (req, res) {
+server.post('/api/v1/email', function (req, res) {
 	postmark.sendEmail({
 		From: 'rsvp@rstr.io',
 		To: 'murray@mismith.info',
@@ -23,6 +24,16 @@ server.post('/api/v1/notify/email', function (req, res) {
 			return;
 		}
 		res.json({success: true});
+	});
+});
+server.get('/api/v1/email/reminder', function (req, res) {
+	fs.readFile('emails/reminder.html', function (err, data) {
+		if (err) {
+			console.error(err);
+			return;
+		}
+		res.type('text/html');
+		res.send(data);
 	});
 });
 
