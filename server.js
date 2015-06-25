b6b8486ea553a0ef140478beaef1ef1d39162ea3
Murray Smith <murray@businessinstincts.com>
2015-06-24 18:08:01 -0600

    WIP: batch reminder emailing

--- server.js
+++ server.js
@@ -10,99 +10,123 @@ var fs       = require('fs'),
 	ejs      = require('ejs'),
 	juice    = require('juice'),
 	moment   = require('moment'),
-	Firebase = require('firebase');
+	Firebase = require('firebase'),
+	merge    = require('object-merge');
 
 // web app
 server.use(express.static('html'));
 server.get('/', express.static('html/index.html'));
 
 // api
-server.post('/api/v1/email', function (req, res) {
-	postmark.sendEmail({
-		From: 'rsvp@rstr.io',
-		To: 'murray@mismith.info',
-		Subject: 'Test', 
-		TextBody: 'Hello from Postmark!',
-		TrackOpens: true,
-	}, function (err, data) {
-		if (err) {
-			res.json({error: err});
-			return;
-		}
-		res.json({success: true});
-	});
-});
-server.get('/api/v1/email/:template', function (req, res) {
+
+function getCompiledTemplate(templateId, callback) {
 	// read template
-	fs.readFile('html/emails/' + req.params.template + '.html', function (err, file) {
+	fs.readFile('html/emails/' + templateId + '.html', function (err, file) {
 		if (err) {
 			console.error(err);
 			return;
 		}
 		
-		// fetch data
-		new Firebase(FB_BASE_URL + '/rosters/' + req.query.roster).on('value', function (rosterSnap) {
-			var roster  = rosterSnap.val(),
-				event   = roster.events[req.query.event],
-				subject = 'Reminder: RSVP Required - ' + roster.name + ' - ' + event.name;
-			
-			// setup special vars
-			roster.url = '#/roster/' + req.query.roster;
-			event.url  = roster.url + '/' + req.query.event;
-			
-			// render template
-			var template = ejs.compile(file.toString());
-			var html = template({
-				subject: subject,
-				roster:  roster,
-				event:   event,
-				
-				user: {
-					email: 'murray@mismith.info',
-				},
-				
-				url:     '#/roster/' + req.query.roster + '/' + req.query.event,
-				query:   req.query,
-				moment:  moment,
-			});
-			
-			// juice email
-			juice.juiceResources(html, {}, function (err, juiced) {
-				if (err) {
-					console.error(err);
-					return;
-				}
-				
-				if (1) {
-					html = juiced;
-				}
-				if (1) {
-					// gather recipients
-					var to = 'Murray Smith <murray@mismith.info>';
+		// compile it
+		var compiled = ejs.compile(file.toString());
+		
+		// return it
+		callback(compiled);
+	});
+}
+function getEventInfo(rosterId, eventId, callback) {
+	// fetch data
+	new Firebase(FB_BASE_URL + '/rosters/' + rosterId).on('value', function (snapshot) {
+		var roster  = snapshot.val(),         // @TODO: make sure this roster exists
+			event   = roster.events[eventId]; // @TODO: make sure this event exists
+		
+		roster.$id = rosterId;
+		roster.url = '#/roster/' + rosterId;
+		event.$id  = eventId;
+		event.url  = roster.url + '/' + eventId;
+		
+		callback(roster, event);
+	});
+}
+function getJuicedEmail(template, data, callback) {
+	// render email
+	var html = template(data);
+	
+	// juice email
+	juice.juiceResources(html, {}, function (err, juiced) {
+		if (err) {
+			console.error(err);
+			return;
+		}
+		
+		callback(juiced);
+	});
+}
+
+server.get('/api/v1/email/reminder', function (req, res) {
+	getCompiledTemplate('reminder', function (template) {
+		getEventInfo(req.query.roster, req.query.event, function (roster, event) {
+			var subject = 'Reminder: RSVP Required - ' + roster.name + ' - ' + event.name,
+				data = {
+					subject:  subject,
+					roster:   roster,
+					event:    event,
 					
-					// send email
-					postmark.sendEmail({
-						From: 'rsvp@rstr.io',
-						To: to,
-						Subject: subject, 
-						HtmlBody: html,
-						TrackOpens: true,
-					}, function (err, data) {
-		/*
-						if (err) {
-							res.json({error: err});
-							return;
-						}
-						res.json({success: true});
-		*/
-					});
-				}
-				
-				// return response
+					// helpers
+					moment:   moment,
+				};
+			
+			getJuicedEmail(template, data, function (html) {
+				// display email
 				res.type('text/html');
 				res.send(html);
 			});
+		});
+	});
+});
+server.post('/api/v1/email/reminder', function (req, res) {
+	getCompiledTemplate('reminder', function (template) {
+		getEventInfo(req.query.roster, req.query.event, function (roster, event) {
+			var subject = 'Reminder: RSVP Required - ' + roster.name + ' - ' + event.name,
+				data = {
+					subject:  subject,
+					roster:   roster,
+					event:    event,
+					
+					// helpers
+					moment:   moment,
+				},
+				response = {success: true};
 			
+			Object.keys(roster.participants).forEach(function (userId) {
+				var user = merge(roster.participants[userId]); // @TODO / @FIX: get real user
+				
+				if (user.email && ( ! event.rsvps || ! event.rsvps[userId] || event.rsvps[userId].status < 0)) {
+					console.log(user, data);
+					var userData = merge(data, {user: user});
+					
+					getJuicedEmail(template, userData, function (html) {
+						// send email
+						postmark.sendEmail({
+							From:       'rsvp@rstr.io',
+							To:         user.name + ' <' + user.email + '>',
+							Subject:    subject, 
+							HtmlBody:   html,
+							TrackOpens: true,
+						}, function (err) {
+							if (err) {
+								response.failed.push({data: user, error: err});
+								response.success = false;
+								return;
+							}
+							response.sent.push({data: user});
+						});
+					});
+					
+				}
+			});
+			
+			res.json({success: true});
 		});
 	});
 });
